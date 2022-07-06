c14b12b61084e901be9b8a716b2223bf401a35ba
Joseph Darville <joseph.darville9@gmail.com>
2022-07-06 19:23:57 -0400

    cleaned up code

--- server.js
+++ server.js
@@ -1,59 +1,15 @@
 const express = require('express');
 const PORT = process.env.PORT || 3002
 const app = express();
-const notes = require('./db/db');
-const fs = require('fs');
-//node API makes working with fs more predictable
-// provides utils for working with file and dir paths
 const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-
-function findById(id, notesArray) {
-    const result = notesArray.filter(note = note.id === id) [0];
-    return result;
-}
-
-function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note)
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notes: notesArray}, null, 2)
-    );
-    return note;
-};
-
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-    res.json(results);
-});
-
-app.get('/api/notes/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404)
-    }
-});
-
-app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-    const note = createNewNote(req.body, notes)
-
-    res.json(req.body);
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
