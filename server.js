c76b91b235969c034d44cc9980ff031ec1ab8331
Joseph Darville <joseph.darville9@gmail.com>
2022-07-05 23:43:11 -0400

    notes now populate and have id assignment

--- server.js
+++ server.js
@@ -1,32 +1,48 @@
 const express = require('express');
 const PORT = process.env.PORT || 3002
 const app = express();
-const { notes } = require('./db/db');
+const notes = require('./db/db');
 const fs = require('fs');
 //node API makes working with fs more predictable
 // provides utils for working with file and dir paths
 const path = require('path');
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(note = note.id === id) [0];
+    return result;
+}
 
 function createNewNote(body, notesArray) {
-    const newNote = body;
-    notesArray.push(newNote)
+    const note = body;
+    notesArray.push(note)
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
         JSON.stringify({ notes: notesArray}, null, 2)
     );
-    return newNote;
+    return note;
 };
 
-app.get('api/notes', (req, res) => {
-    res.json(notes);
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
 });
 
-app.post('api/notes', (req, res) => {
-    const newNote = createNewNote(req.body, notes)
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404)
+    }
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes)
 
     res.json(req.body);
 });
